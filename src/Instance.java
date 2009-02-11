b32cb1a3158d36a09a014c374876c5c680160e1a
esbie <esbie@192.168.1.104>
2009-02-10 22:46:46 -0500

    added Instance

---
+++ src/Instance.java
@@ -0,0 +1,13 @@
+
+public class Instance {
+
+	public String target;
+	public int[] senseids;
+	public String[] collocation;
+
+	public Instance(String target, int[] senseids, String[] collocation){
+		this.target = target;
+		this.senseids = senseids;
+		this.collocation = collocation;
+	}
+}
